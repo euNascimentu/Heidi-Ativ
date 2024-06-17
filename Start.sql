ebc84566d6fe78a1bad499730cdc681d0ce68b17
Fernando <fernando.nprimo@senacsp.edu.br>
2024-06-17 16:56:46 -0300

    Inserindo registro Recepcionista e Paciente

--- Start.sql
+++ Start.sql
@@ -65,3 +65,86 @@ REFERENCES Recepcionista(idRecepcionista)
 );
 
 SHOW TABLES /*mostra as tabelas do banco de dados selecionado*/
+
+/*Inserindo dois ou mais registros*/
+INSERT INTO medico (nomeMedico,loginMedico,senha,crm)
+VALUES ('Dra.Rosana','rosana@gmail.com','12345678','666333MG');
+ 
+INSERT INTO medico (nomeMedico,loginMedico,senha,crm)
+VALUES ('Dr.Heitor','heitor@gmail.com','12345678','555333SP');
+ 
+/*Filtro e Operadores*/
+SELECT idMedico, nomeMedico, CRM FROM medico
+WHERE idMedico >= 2 AND idMedico <= 5 
+
+/*Filtros com between*/
+SELECT idMedico, nomeMedico, CRM FROM medico
+WHERE idMedico BETWEEN 2 AND 5 
+ 
+INSERT INTO medico (nomeMedico,loginMedico,senha,crm)
+VALUES  ('Dr.Ricardo','ricardo@gmail.com','12345678','777222MG'),
+('Dr.Graziella','graziella@gmail.com','12345678','555111RJ');
+ 
+SELECT nomeMedico,crm FROM medico /*Escolhendo algumas colunas*/
+SELECT * FROM medico /*Trazendo todas as colunas*/
+ 
+/*Insert Explícito*/
+INSERT INTO medico (crm, nomeMedico,senha,loginMedico)
+VALUES ('666111SP','Dra Ana Maria de novo','','anamaria@gmail.com');
+
+UPDATE medico
+SET senha= 14022005, 
+	loginMedico= 'anamariabrog@gmail.com'
+WHERE idMedico = 7;
+
+/*Insert Implícito*/
+INSERT INTO medico
+VALUES (6,'Dr.Sergio Malandro','serginhomala@gmail.com','12332122','123456SP');
+
+/*Excluir Registros*/
+DELETE FROM medico
+WHERE idMedico = 6
+
+/*Filtro com like*/
+SELECT crm, nomeMedico, loginMedico FROM medico
+where loginMedico LIKE 'an%'
+
+/*Ordenando resultados de uma consulta*/
+SELECT * FROM medico
+ORDER BY loginMedico ASC /*crescente*/
+
+SELECT * FROM medico
+ORDER BY loginMedico ASC /*decrescente*/
+
+SELECT * FROM paciente
+
+SELECT * FROM recepcionista
+
+/*Inserindo 3 pacientes*/
+INSERT INTO paciente (nome, dataNascimento, cpf, email, tipoSanguineo, doencaPaciente)
+VALUES ('Josefino Pereira', '1964-05-3 06:37','57863407902', 'josefino@gmail.com', 'O+', 'Tuberculose')
+
+INSERT INTO paciente (nome, dataNascimento, cpf, tipoSanguineo)
+VALUES ('Marizete Silva','1953-02-14 12:30:18','12398764503','AB-')
+
+INSERT INTO paciente (nome, dataNascimento, cpf, email, tipoSanguineo, doencaPaciente)
+VALUES ('Matheus Marani', '2006-12-06 21:58:32', '45689635712', 'matheusmarani12@gmail.com', 'A+', 'Câncer Terminal Pulmonar')
+
+UPDATE paciente
+SET
+WHERE idPaciente =
+
+/*Inserindo 3 recepcionistas*/
+
+UPDATE recepcionista
+SET cidade = 'São Vicente'
+WHERE idRecepcionista = 2
+
+INSERT INTO recepcionista (nomeRecepcionista, loginRecepcionista, senha, celular, nomeLogradouro, numero, complemento, cidade, cep, estado)
+VALUES ('Fabiana Nascimento', 'fabiananascimento', '14021984', '13974118720', 'Conselheiro Nébias', '309', 'Apart.', 'Santos', '11015-003', 'SP')
+
+INSERT INTO recepcionista (nomeRecepcionista, loginRecepcionista, senha, celular, nomeLogradouro, numero, complemento, cidade, cep, estado)
+VALUES ('Kauan Quaresma', 'kauanbeck', 'eujgolol', '13974586234', 'Carijós', '23', 'Casa', 'São Vicente', '11687698', 'SP')
+
+INSERT INTO recepcionista (nomeRecepcionista, loginRecepcionista, senha, celular, nomeLogradouro, numero, complemento, cidade, cep, estado)
+VALUES ('Arley Siaci', 'arleydocente', 'arley123', '13974235487', 'Guarani', '574', 'Casa', 'São Vicente', '11654895', 'SP')
